5e56e944abdee954c44e76c00db2559f9094cbe7
Bryson G <114206517+bryson-g@users.noreply.github.com>
2024-02-08 12:40:51 -0800

    add input fields

--- src/app/plan/create-meal.tsx
+++ src/app/plan/create-meal.tsx
@@ -1,6 +1,18 @@
 import { Button } from "@/components/ui/button";
 import CaloriePicker from "@/components/ui/calorie-picker";
-import { Drawer, DrawerClose, DrawerContent, DrawerDescription, DrawerFooter, DrawerHeader, DrawerTitle, DrawerTrigger } from "@/components/ui/drawer";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
 import { PlusCircle } from "lucide-react";
 import { useState } from "react";
 
@@ -15,14 +27,20 @@ export default function CreateMeal() {
         </Button>
       </DrawerTrigger>
       <DrawerContent>
-        <div className="mx-auto w-full max-w-sm">
+        <div className="mx-auto w-full max-w-sm flex flex-col items-center">
           <DrawerHeader>
             <DrawerTitle>create meal</DrawerTitle>
             <DrawerDescription>
               set name, calories, and ingredients of your meal
             </DrawerDescription>
           </DrawerHeader>
+
+          {/* // todo : fix styling here */}
+          <Label htmlFor="mealName"> Meal Name </Label>
+          <Input placeholder="meal name" id="mealName" />
+          <Textarea placeholder="seperate ingredients by comma: turkey, cheese, etc" />
           <CaloriePicker calories={calories} setCalories={setCalories} />
+
           <DrawerFooter className="mt-5">
             <Button>Submit</Button>
             <DrawerClose asChild>
